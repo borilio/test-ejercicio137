b7ed57b15bbbfc717e8ee15d5f89de2a5386e8c0
borilio <exprimetuandroid@gmail.com>
2023-05-31 12:18:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Testing de la clase Dado
+
+Aquí haremos las pruebas unitarias de la clase Dado.
+
+## Tareas
+
+1. Añadir las dependencias de JUnit al pom.xml
+2. Próximamente...
+
+
+## Pruebas Unitarias
+
---
+++ src/main/java/com/inserta/Dado.java
@@ -0,0 +1,55 @@
+package com.inserta;
+
+import java.util.Random;
+
+public class Dado {
+	
+	//Atributos
+	private int caras;
+	private int acumulado;
+	
+	//Constructores
+	public Dado() {
+		this.caras = 6;
+		this.acumulado = 0;
+	}
+	
+	public Dado(int numeroCaras) {
+		this.caras = numeroCaras;
+		this.acumulado = 0;
+	}
+	
+	
+	//Métodos
+	public int lanzar() {
+		Random aleatorio = new Random();
+		//Guardamos la tirada aleatoria
+		int tirada = aleatorio.nextInt(1, caras+1);
+		//La acumulamos en el atributo
+		acumulado += tirada;
+		
+		return tirada;
+	}
+	
+	public char letra() {
+		Random r = new Random();
+		// Un char se puede inicializar con un 'A' o con un 65. Así que lo inicializamos con un número aleatorio entre 65 y 90
+		return (char) r.nextInt(65, 91);
+	}
+
+	public int getCaras() {
+		return caras;
+	}
+
+	public void setCaras(int caras) {
+		this.caras = caras;
+	}
+
+	@Override
+	public String toString() {
+		return "Dado{" +
+				"caras=" + caras +
+				", acumulado=" + acumulado +
+				'}';
+	}
+}
